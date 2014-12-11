603df8fcd5d742bd4d77474dc1a4df2704460a0e
Garrigueta <ferran.garriga@gmail.com>
2014-12-11 13:55:27 +0100

    Added project
    added vs2013(community) project sources

---
+++ WebInteractCSharpWinForm/WebserverLoader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebInteractCSharpWinForm
+{
+    class WebserverLoader
+    {
+        public WebserverLoader()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo(System.IO.Directory.GetCurrentDirectory() + "\\babyweb.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Minimized;
+            startInfo.Arguments = "//Config:.\babyweb.ini";
+            Process.Start(startInfo);
+
+            //startInfo.Arguments = "/Config:.\babyweb.ini";
+
+            //Process.Start(startInfo);
+            //MessageBox.Show(Application.StartupPath);
+        }
+    }
+}
